d8401bcd64752f5d9e50ace8b379e4edc0fe4ca8
Graham <gmcallister@gatech.edu>
2015-06-28 17:33:54 -0400

    Need to make trie and finish GUI

--- src/Entry.java
+++ src/Entry.java
@@ -1,4 +1,4 @@
-import java.time.LocalDateTime
+import java.time.LocalDateTime;
 
 /**
  * @author Graham McAllister
@@ -20,6 +20,22 @@ public class Entry {
         created = LocalDateTime.now();
     }
 
-    public void edit(String )
+    /**
+     * Replaces the text of Entry with the newText parameter
+     * @param newText           new body text for Entry
+     */
+    public void edit(String newText) {
+        body = newText;
+    }
+
+    /**
+     * Replaces the title of Entry with the newTitle parameter
+     * @param newTitle          new title for Entry
+     */
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+
 
 }
--- src/MyFirstTrie.java
+++ src/MyFirstTrie.java
@@ -1,9 +1,15 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.collections.FXCollections;
 
 
 /**
@@ -15,13 +21,51 @@ import javafx.scene.control.TableView;
 public class MyFirstTrie extends Application {
 
     //Instance variables
-    private ObservableList<Entry> entryData = new ObservableList();
+
+    /** tableView data for entries */
+    private ObservableList<Entry> entryData = FXCollections.observableArrayList();
+    /** tableView for entries */
+    private TableView entryList;
+
+    /** Buttons */
+    private Button sortDate;
+    private Button search;
+
+    /** Labels and other control elements */
+    private Label title;
+    private TextField searchBar;
+    private TextArea body;
+
+    /** Panels and other scene elements */
+    private HBox root;
+    private VBox lower;
+    private HBox navigationBar;
 
     /**
      * Application viewer controller
      * @param primaryStage            GUI application window
      */
     public void start(Stage primaryStage) {
+
+        //Instantiating scene elements
+        entryList = new TableView(entryData);
+        sortDate = new Button("Sort by Date");
+        search = new Button("Search");
+        title = new Label("Title: ");
+        searchBar = new TextField();
+        body = new TextArea();
+        lower = new VBox();
+        navigationBar = new HBox();
+        root = new HBox();
+        lower.getChildren().addAll(entryList, body);
+        navigationBar.getChildren().addAll(sortDate, searchBar);
+        root.getChildren().addAll(lower, navigationBar);
+
+
+        //Formatting scene elements
+        searchBar.setPromptText("Search");
+
+
 
         Scene rootScene = new Scene(root, 800, 500);
         primaryStage.setTitle("MyFirstTrie - A diary application by Graham McAllister");
